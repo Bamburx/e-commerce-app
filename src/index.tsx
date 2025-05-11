baa4c69de8e91847761441b45dd012c97f79d762
Bamburic <michalbamburski@gmail.com>
2025-05-11 21:54:34 +0200

    Fix: Set correct basename for new repo

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/e-commerce-shop">
+    <BrowserRouter basename="/e-commerce-app/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
